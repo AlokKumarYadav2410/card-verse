3326f3aba729dcdeaf037e1675bf15caf89c67ae
SubastralAlok <alokkumaryadav241004@gmail.com>
2025-12-25 17:28:04 +0530

    Completed Card-Verse

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Cards from './Components/Cards'
+import { mixedCharacters } from './Data/mixedCharacters'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="w-full min-h-screen bg-linear-to-r from-sky-950 via-sky-800 to-sky-100 flex justify-center gap-6 p-10 flex-wrap">
+      {mixedCharacters.map((character) => (
+        <Cards key={character.id} characters={character} />
+      ))}
     </div>
   )
 }
